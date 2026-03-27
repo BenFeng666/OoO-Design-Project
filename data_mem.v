422fa1a1d73b850e644d090650dc5d7683b5c1ab
BenFeng666 <zhibinfeng2017@gmail.com>
2026-03-26 23:52:03 -0400

    Update data_mem.v

--- data_mem.v
+++ data_mem.v
@@ -8,36 +8,27 @@ module data_mem (
     output reg [31:0] read_data
 );
 
-reg [31:0] data_mem [256];
+reg [31:0] data_mem [0:255];
 integer i;
 
-always @(posedge clk or negedge rst)
-begin
-    
-
-    if (!rst)
-    begin
-        for (i=0; i<256; i++)
-        begin
-            data_mem[i]<=32'b0;
-        end
-        read_data<=32'b0;
+// synchronous write, async reset
+always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+        for (i = 0; i < 256; i = i + 1)
+            data_mem[i] <= 32'b0;
     end
     else begin
-
         if (mem_write)
-        begin
             data_mem[address[9:2]] <= write_data;
-        end
-
-        if (mem_read)
-        begin
-            read_data  <= data_mem [address[9:2]];
-        end
     end
-
-
 end
 
+// combinational read
+always @(*) begin
+    if (mem_read)
+        read_data = data_mem[address[9:2]];
+    else
+        read_data = 32'b0;
+end
 
 endmodule
