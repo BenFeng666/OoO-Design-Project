74f466550e8b3a817ebdbef225527cda3d743a04
BenFeng666 <zhibinfeng2017@gmail.com>
2026-03-26 23:52:24 -0400

    Update instruction_mem.v

--- instruction_mem.v
+++ instruction_mem.v
@@ -3,9 +3,24 @@ module instruction_mem(
     output reg [31:0] instruction
 );
 
-reg [31:0] inst_mem [256];
+reg [31:0] inst_mem [0:255];
+integer i;
+
+initial begin
+    // clear all memory first
+    for (i = 0; i < 256; i = i + 1)
+        inst_mem[i] = 32'b0;
+
+    // program
+    inst_mem[0] = 32'h00500093; // x1 = 5
+    inst_mem[1] = 32'hFFD00113; // x2 = -3
+    inst_mem[2] = 32'h002081b3; // x3 = x1 + x2
+    
+
+end
 
 always @(*) begin
-    instruction = inst_mem[addr];
+    instruction = inst_mem[addr[9:2]];
 end
+
 endmodule
