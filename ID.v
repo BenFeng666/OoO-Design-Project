a1341d9fdede0cb8d6d38349b3aa4522d9f121d7
BenFeng666 <zhibinfeng2017@gmail.com>
2026-03-31 19:45:47 -0400

    Create ID.v

---
+++ ID.v
@@ -0,0 +1,62 @@
+module ID_EX (
+    input wire clk,
+    input wire rst,
+    input wire [31:0] rs1,
+    input wire [31:0] rs2,
+    input wire [31:0] imm,
+    input wire ALUSrc,
+    input wire MemRead,
+    input wire MemWrite,
+    input wire MemtoReg,
+    input wire Branch,
+    input wire jump,
+    input [3:0] ctrl,
+    output reg [31:0] ex_rs1,
+    output reg [31:0] ex_rs2,
+    output reg [31:0] ex_imm,
+    output reg [3:0] ex_ctrl,
+    output reg ex_ALUSrc,
+    output reg ex_MemRead,
+    output reg ex_MemWrite,
+    output reg ex_MemtoReg,
+    output reg ex_Branch,
+    output reg ex_jump
+
+);
+
+
+
+always @(posedge clk or negedge rst) begin
+    if (!rst)
+    begin
+        ex_rs1 <= 32'b0;
+        ex_rs2 <= 32'b0;
+        ex_imm <= 32'b0;
+        ex_ctrl <= 4'b0;
+        ex_ALUSrc <=1'b0;
+        ex_MemRead <=1'b0;
+        ex_MemWrite <=1'b0;
+        ex_MemtoReg <=1'b0;
+        ex_Branch <=1'b0;
+        ex_jump <=1'b0;
+
+    end
+
+    else 
+    begin
+        ex_rs1 <= rs1;
+        ex_rs2 <= rs2;
+        ex_imm <= imm;
+        ex_ctrl <= ctrl;
+        ex_ALUSrc <= ALUSrc;
+        ex_MemRead <= MemRead;
+        ex_MemWrite <= MemWrite;
+        ex_MemtoReg <= MemtoReg;
+        ex_Branch <= Branch;
+        ex_jump <= jump;
+
+    end
+
+
+end
+endmodule
