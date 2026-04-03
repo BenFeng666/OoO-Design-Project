62725fb09d6bbdf60016c26c9783e42c319ba5e9
BenFeng666 <zhibinfeng2017@gmail.com>
2026-04-02 22:25:56 -0400

    Update ALU_control.v

--- ALU_control.v
+++ ALU_control.v
@@ -94,12 +94,58 @@ always @(*) begin
     end
     end
 
-    7'b0100011:; // don't support load in ALU yet
+        7'b0000011: begin // load
+        ctrl=4'b0000; // add, address = rs1 + imm
+    end
 
-    7'b1100011:; // don't support B type yet
-    7'b0110111:; // don't support J type yet
-    7'b1101111:; // don't support U type yet
-    default: ctrl = 4'b0000 ;
+    7'b0100011: begin // S type
+        ctrl=4'b0000; // add, address = rs1 + imm
+    end
+
+    7'b1100011: begin // B type
+        if (instruction[14:12]==3'b000)
+        begin
+            ctrl=4'b0001; // beq -> sub
+        end
+        else if (instruction[14:12]==3'b001)
+        begin
+            ctrl=4'b0001; // bne -> sub
+        end
+        else if (instruction[14:12]==3'b100)
+        begin
+            ctrl=4'b1000; // blt -> less than signed
+        end
+        else if (instruction[14:12]==3'b101)
+        begin
+            ctrl=4'b1000; // bge -> less than signed
+        end
+        else if (instruction[14:12]==3'b110)
+        begin
+            ctrl=4'b1001; // bltu -> less than unsigned
+        end
+        else if (instruction[14:12]==3'b111)
+        begin
+            ctrl=4'b1001; // bgeu -> less than unsigned
+        end
+    end
+
+    7'b1101111: begin // J type jal
+        ctrl=4'b0000; // add
+    end
+
+    7'b1100111: begin // jalr
+        ctrl=4'b0000; // add
+    end
+
+    7'b0110111: begin // U type lui
+        ctrl=4'b1010; // pass imm, need ALU support
+    end
+
+    7'b0010111: begin // U type auipc
+        ctrl=4'b0000; // add, PC + imm
+    end
+
+    default: ctrl = 4'b0;
     
 
     endcase
