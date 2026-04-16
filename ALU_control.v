8bbffd100772aaf4c7b4375ca9c84177c3ee1f83
BenFeng666 <zhibinfeng2017@gmail.com>
2026-04-15 23:53:48 -0400

    Refactor ALU control logic for clarity and organization

--- ALU_control.v
+++ ALU_control.v
@@ -4,156 +4,108 @@ module ALU_ctrl(
 );
 
 always @(*) begin
-    ctrl=4'b0000;
+    ctrl = 4'b0000;
+
     case (instruction[6:0])
-    7'b0110011: begin// R type
 
-    if (instruction[14:12]==3'b000 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b0000;//add
-    end
-    else if (instruction[14:12]==3'b000 && instruction[31:25]==7'b0100000)
-    begin
-        ctrl=4'b0001;//sub
-    end
-    else if (instruction[14:12]==3'b100 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b0100;//^
-    end 
-    else if (instruction[14:12]==3'b000 && instruction[31:25]==7'b0000001)
-    begin
-    ctrl=4'b1011; // mul
-    end
-    else if (instruction[14:12]==3'b110 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b0011;//or
-    end
-    else if (instruction[14:12]==3'b111 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b0010; //and
-    end
-    else if (instruction[14:12]==3'b001 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b0101; //shift left
-    end
-    else if (instruction[14:12]==3'b101 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b0110; //shift right
-    end
-    else if (instruction[14:12]==3'b101 && instruction[31:25]==7'b0100000)
-    begin
-        ctrl=4'b0111; //shift righ msb extend
-    end
-    else if (instruction[14:12]==3'b010 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b1000; //less than signed
-    end
-    else if (instruction[14:12]==3'b011 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b1001; //less than unsign
-    end
-
-    end
-
-    7'b0010011: begin //I type
-    if (instruction[14:12]==3'b000)
-    begin
-        ctrl=4'b0000;//add
-    end
-    //else if (instruction[14:12]==3'b000 && instruction[31:25]==7'b0010100)
-    //begin
-        //ctrl=4'b0001;//sub
-    //end
-    else if (instruction[14:12]==3'b100)
-    begin
-        ctrl=4'b0100;//^
-    end 
-    else if (instruction[14:12]==3'b110)
-    begin
-        ctrl=4'b0011;//or
-    end
-    else if (instruction[14:12]==3'b111)
-    begin
-        ctrl=4'b0010; //and
-    end
-    else if (instruction[14:12]==3'b001 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b0101; //shift left
-    end
-    else if (instruction[14:12]==3'b101 && instruction[31:25]==7'b0000000)
-    begin
-        ctrl=4'b0110; //shift right
-    end
-    else if (instruction[14:12]==3'b101 && instruction[31:25]==7'b0100000)
-    begin
-        ctrl=4'b0111; //shift righ msb extend
-    end
-    else if (instruction[14:12]==3'b010 )
-    begin
-        ctrl=4'b1000; //less than signed
-    end
-    else if (instruction[14:12]==3'b011)
-    begin
-        ctrl=4'b1001; //less thanunsigned
-    end
-    end
-
-        7'b0000011: begin // load
-        ctrl=4'b0000; // add, address = rs1 + imm
-    end
-
-    7'b0100011: begin // S type
-        ctrl=4'b0000; // add, address = rs1 + imm
-    end
-
-    7'b1100011: begin // B type
-        if (instruction[14:12]==3'b000)
-        begin
-            ctrl=4'b0001; // beq -> sub
+        // R-type
+        7'b0110011: begin
+            if (instruction[14:12] == 3'b000 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b0000; // add
+            else if (instruction[14:12] == 3'b000 && instruction[31:25] == 7'b0100000)
+                ctrl = 4'b0001; // sub
+            else if (instruction[14:12] == 3'b100 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b0100; // xor
+            else if (instruction[14:12] == 3'b000 && instruction[31:25] == 7'b0000001)
+                ctrl = 4'b1011; // mul
+            else if (instruction[14:12] == 3'b110 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b0011; // or
+            else if (instruction[14:12] == 3'b111 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b0010; // and
+            else if (instruction[14:12] == 3'b001 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b0101; // sll
+            else if (instruction[14:12] == 3'b101 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b0110; // srl
+            else if (instruction[14:12] == 3'b101 && instruction[31:25] == 7'b0100000)
+                ctrl = 4'b0111; // sra
+            else if (instruction[14:12] == 3'b010 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b1000; // slt
+            else if (instruction[14:12] == 3'b011 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b1001; // sltu
         end
-        else if (instruction[14:12]==3'b001)
-        begin
-            ctrl=4'b0001; // bne -> sub
+
+        // I-type arithmetic
+        7'b0010011: begin
+            if (instruction[14:12] == 3'b000)
+                ctrl = 4'b0000; // addi
+            else if (instruction[14:12] == 3'b100)
+                ctrl = 4'b0100; // xori
+            else if (instruction[14:12] == 3'b110)
+                ctrl = 4'b0011; // ori
+            else if (instruction[14:12] == 3'b111)
+                ctrl = 4'b0010; // andi
+            else if (instruction[14:12] == 3'b001 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b0101; // slli
+            else if (instruction[14:12] == 3'b101 && instruction[31:25] == 7'b0000000)
+                ctrl = 4'b0110; // srli
+            else if (instruction[14:12] == 3'b101 && instruction[31:25] == 7'b0100000)
+                ctrl = 4'b0111; // srai
+            else if (instruction[14:12] == 3'b010)
+                ctrl = 4'b1000; // slti
+            else if (instruction[14:12] == 3'b011)
+                ctrl = 4'b1001; // sltiu
         end
-        else if (instruction[14:12]==3'b100)
-        begin
-            ctrl=4'b1000; // blt -> less than signed
+
+        // load
+        7'b0000011: begin
+            ctrl = 4'b0000; // address = rs1 + imm
         end
-        else if (instruction[14:12]==3'b101)
-        begin
-            ctrl=4'b1000; // bge -> less than signed
+
+        // store
+        7'b0100011: begin
+            ctrl = 4'b0000; // address = rs1 + imm
         end
-        else if (instruction[14:12]==3'b110)
-        begin
-            ctrl=4'b1001; // bltu -> less than unsigned
+
+        // branches
+        7'b1100011: begin
+            if (instruction[14:12] == 3'b000)
+                ctrl = 4'b0001; // beq -> subtract compare
+            else if (instruction[14:12] == 3'b001)
+                ctrl = 4'b0001; // bne -> subtract compare
+            else if (instruction[14:12] == 3'b100)
+                ctrl = 4'b1000; // blt
+            else if (instruction[14:12] == 3'b101)
+                ctrl = 4'b1000; // bge uses signed compare too
+            else if (instruction[14:12] == 3'b110)
+                ctrl = 4'b1001; // bltu
+            else if (instruction[14:12] == 3'b111)
+                ctrl = 4'b1001; // bgeu uses unsigned compare too
         end
-        else if (instruction[14:12]==3'b111)
-        begin
-            ctrl=4'b1001; // bgeu -> less than unsigned
+
+        // jal
+        7'b1101111: begin
+            ctrl = 4'b0000;
         end
-    end
 
-    7'b1101111: begin // J type jal
-        ctrl=4'b0000; // add
-    end
+        // jalr
+        7'b1100111: begin
+            ctrl = 4'b0000;
+        end
 
-    7'b1100111: begin // jalr
-        ctrl=4'b0000; // add
-    end
+        // lui
+        7'b0110111: begin
+            ctrl = 4'b1010;
+        end
 
-    7'b0110111: begin // U type lui
-        ctrl=4'b1010; // pass imm, need ALU support
-    end
+        // auipc
+        7'b0010111: begin
+            ctrl = 4'b0000;
+        end
 
-    7'b0010111: begin // U type auipc
-        ctrl=4'b0000; // add, PC + imm
-    end
-
-    default: ctrl = 4'b0;
-    
-
+        default: begin
+            ctrl = 4'b0000;
+        end
     endcase
-
-
 end
+
 endmodule
