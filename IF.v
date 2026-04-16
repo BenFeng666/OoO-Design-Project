24e0db9e687caf8bd09579b988cf9c38f28ed0d3
BenFeng666 <zhibinfeng2017@gmail.com>
2026-04-16 04:24:20 -0400

    Refactor IF_ID module to handle flush and stall

--- IF.v
+++ IF.v
@@ -1,30 +1,37 @@
 module IF_ID (
     input wire clk,
     input wire rst,
+    input wire flush,
+    input wire stall,
     input wire [31:0] PC,
-    input wire [31:0]instruction,
+    input wire [31:0] instruction,
     input wire [31:0] PC_4,
     output reg [31:0] ID_instruction,
     output reg [31:0] ID_PC,
     output reg [31:0] ID_PC_4
 );
 
-
-
 always @(posedge clk or negedge rst) begin
-    if (!rst)
-    begin
-       ID_instruction <= 32'b0;
-       ID_PC <= 32'b0;
-       ID_PC_4 <= 32'b0; 
+    if (!rst) begin
+        ID_instruction <= 32'b0;
+        ID_PC <= 32'b0;
+        ID_PC_4 <= 32'b0;
     end
-    else 
-    begin
-    ID_instruction <= instruction;
-    ID_PC <= PC;
-    ID_PC_4 <= PC_4;
+    else if (flush) begin
+        ID_instruction <= 32'h00000013; // NOP
+        ID_PC <= 32'b0;
+        ID_PC_4 <= 32'b0;
     end
-
-
+    else if (stall) begin
+        ID_instruction <= ID_instruction;
+        ID_PC <= ID_PC;
+        ID_PC_4 <= ID_PC_4;
+    end
+    else begin
+        ID_instruction <= instruction;
+        ID_PC <= PC;
+        ID_PC_4 <= PC_4;
+    end
 end
+
 endmodule
